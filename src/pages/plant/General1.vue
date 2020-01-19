9edc414c7da5acfa34be13f7df6faabad7a386cb
Vitaliy Savenets <vitaliy.savenets@gmail.com>
2020-01-19 23:58:28 +0300

    plants by category

--- src/pages/plant/General1.vue
+++ src/pages/plant/General1.vue
@@ -1,7 +1,16 @@
 <template>
   <q-page>
-    Test {{ $route.params.id }}
-    !!!{{data}}!!!
+    <div v-if="$route.params.id">
+      <div v-for="item in data" v-bind:key="item.id" style="border-bottom: 2px solid black">
+        {{item}}
+      </div>
+      <div v-if="!data.length">
+        Not Found!
+      </div>
+    </div>
+    <div v-else>
+      News...
+    </div>
   </q-page>
 </template>
 
@@ -13,13 +22,23 @@ export default {
       data: {}
     }
   },
-  async mounted () {
-    try {
-      const response = await this.$axios.get('/catplants/' + this.$route.params.id)
-      this.data = response.data
-    } catch (error) {
-      console.log(error.response.data.message)
+  methods: {
+    async getData () {
+      try {
+        const response = await this.$axios.get('/catplants/' + this.$route.params.id)
+        this.data = response.data
+      } catch (error) {
+        console.log(error.response.data.message)
+      }
     }
+  },
+  watch: {
+    $route (to, from) {
+      this.getData()
+    }
+  },
+  async mounted () {
+    this.getData()
   }
 }
 </script>
